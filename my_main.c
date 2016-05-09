b93d190cfd2b266a15ba8623649342211978c5dd
RongYu <stuy.rong.yu@gmail.com>
2016-05-09 11:30:37 -0400

    work

--- my_main.c
+++ my_main.c
@@ -117,6 +117,7 @@ void first_pass() {
 	exit(0);
       }
       break;
+    
     }
   }
   
@@ -253,7 +254,7 @@ void print_knobs() {
   ====================*/
 void my_main( int polygons ) {
 
-  int i, f, j;
+  int i, f, j, diffFrames;
   double step;
   double xval, yval, zval, knob_value;
   struct matrix *transform;
@@ -266,6 +267,9 @@ void my_main( int polygons ) {
   struct vary_node *vn;
   char frame_name[128];
 
+  s = new_stack();
+  tmp = new_stack();
+
   num_frames = 1;
   step = 5;
  
@@ -273,131 +277,145 @@ void my_main( int polygons ) {
   g.green = 255;
   g.blue = 255;
 
+  first_pass();
+  printf("Completed First Pass \n");
+  printf("Num_Frames: %d\n", num_frames);
+  for (diffFrames = 0; diffFrames<num_frames; diffFrames++){
+    printf("work\n");
     
-  for (i=0;i<lastop;i++) {
-  
-    switch (op[i].opcode) {
-      /*
-	case FRAMES:
-	if (num_frames != 1){
-	printf("Ewww, there's more than one set frame \n");
-	exit(0);
-	}
-	num_frames = op[i].op.frames.num_frames;
-	printf("Frames set to: %d \n", num_frames);
+    for (i=0;i<lastop;i++) {
+      switch (op[i].opcode) {
+      case (1==1):
+	printf("Got here\n");
+      case SPHERE:
+	printf("SPHERE \n");
+	add_sphere( tmp,op[i].op.sphere.d[0], //cx
+		    op[i].op.sphere.d[1],  //cy
+		    op[i].op.sphere.d[2],  //cz
+		    op[i].op.sphere.r,
+		    step);
+	//apply the current top origin
+	matrix_mult( s->data[ s->top ], tmp );
+	draw_polygons( tmp, t, g );
+	tmp->lastcol = 0;
 	break;
-	  
-	case BASENAME:
-	strncpy( name, op[i].op.basename.p, strlen(op[i].op.basename.p));
-	printf("Base Name has been set to: %s \n", op[i].op.basename.p );
+
+      case TORUS:
+	printf("TORUSED\n");
+	add_torus( tmp, op[i].op.torus.d[0], //cx
+		   op[i].op.torus.d[1],     //cy
+		   op[i].op.torus.d[2],    //cz
+		   op[i].op.torus.r0,
+		   op[i].op.torus.r1,
+		   step);
+	matrix_mult( s->data[ s->top ], tmp );
+	draw_polygons( tmp, t, g );
+	tmp->lastcol = 0;
 	break;
-      */
-    case SPHERE:
-      add_sphere( tmp,op[i].op.sphere.d[0], //cx
-		  op[i].op.sphere.d[1],  //cy
-		  op[i].op.sphere.d[2],  //cz
-		  op[i].op.sphere.r,
-		  step);
-      //apply the current top origin
-      matrix_mult( s->data[ s->top ], tmp );
-      draw_polygons( tmp, t, g );
-      tmp->lastcol = 0;
-      break;
 
-    case TORUS:
-      add_torus( tmp, op[i].op.torus.d[0], //cx
-		 op[i].op.torus.d[1],     //cy
-		 op[i].op.torus.d[2],    //cz
-		 op[i].op.torus.r0,
-		 op[i].op.torus.r1,
-		 step);
-      matrix_mult( s->data[ s->top ], tmp );
-      draw_polygons( tmp, t, g );
-      tmp->lastcol = 0;
-      break;
+      case BOX:
+	printf("%d\n",op[i].op.box.d0[0]);
+	printf("%d\n",op[i].op.box.d0[1]);
+	printf("%d\n",op[i].op.box.d0[2]);
+	/*
+	  -1
+	  2147483645
+	  2147483637
+	*/
+	printf("BOXED\n");
 
-    case BOX:
-      add_box( tmp, op[i].op.box.d0[0],
-	       op[i].op.box.d0[1],
-	       op[i].op.box.d0[2],
-	       op[i].op.box.d1[0],
-	       op[i].op.box.d1[1],
-	       op[i].op.box.d1[2]);
-      matrix_mult( s->data[ s->top ], tmp );
-      draw_polygons( tmp, t, g );
-      tmp->lastcol = 0;
-      break;
+	add_box( tmp, op[i].op.box.d0[0],
+		 op[i].op.box.d0[1],
+		 op[i].op.box.d0[2],
+		 op[i].op.box.d1[0],
+		 op[i].op.box.d1[1],
+		 op[i].op.box.d1[2]);
+	printf("BOXED-ed\n");
+	matrix_mult( s->data[ s->top ], tmp );
+	draw_polygons( tmp, t, g );
+	tmp->lastcol = 0;
+	printf("BOXED-ed\n");
+	break;
 
-    case LINE:
-      add_edge( tmp, op[i].op.line.p0[0],
-		op[i].op.line.p0[1],
-		op[i].op.line.p0[1],
-		op[i].op.line.p1[0],
-		op[i].op.line.p1[1],
-		op[i].op.line.p1[1]);
-      draw_lines( tmp, t, g );
-      tmp->lastcol = 0;
-      break;
+      case LINE:
+	printf("LINED\n");
+	add_edge( tmp, op[i].op.line.p0[0],
+		  op[i].op.line.p0[1],
+		  op[i].op.line.p0[1],
+		  op[i].op.line.p1[0],
+		  op[i].op.line.p1[1],
+		  op[i].op.line.p1[1]);
+	draw_lines( tmp, t, g );
+	tmp->lastcol = 0;
+	break;
 
-    case MOVE:
-      //get the factors
-      xval = op[i].op.move.d[0];
-      yval =  op[i].op.move.d[1];
-      zval = op[i].op.move.d[2];
+      case MOVE:
+	printf("MOVE\n");
+	//get the factors
+	xval = op[i].op.move.d[0];
+	yval =  op[i].op.move.d[1];
+	zval = op[i].op.move.d[2];
       
-      transform = make_translate( xval, yval, zval );
-      //multiply by the existing origin
-      matrix_mult( s->data[ s->top ], transform );
-      //put the new matrix on the top
-      copy_matrix( transform, s->data[ s->top ] );
-      free_matrix( transform );
-      break;
+	transform = make_translate( xval, yval, zval );
+	//multiply by the existing origin
+	matrix_mult( s->data[ s->top ], transform );
+	//put the new matrix on the top
+	copy_matrix( transform, s->data[ s->top ] );
+	free_matrix( transform );
+	break;
 
-    case SCALE:
-      xval = op[i].op.scale.d[0];
-      yval = op[i].op.scale.d[1];
-      zval = op[i].op.scale.d[2];
+      case SCALE:
+	printf("SCALE\n");
+	xval = op[i].op.scale.d[0];
+	yval = op[i].op.scale.d[1];
+	zval = op[i].op.scale.d[2];
       
-      transform = make_scale( xval, yval, zval );
-      matrix_mult( s->data[ s->top ], transform );
-      //put the new matrix on the top
-      copy_matrix( transform, s->data[ s->top ] );
-      free_matrix( transform );
-      break;
+	transform = make_scale( xval, yval, zval );
+	matrix_mult( s->data[ s->top ], transform );
+	//put the new matrix on the top
+	copy_matrix( transform, s->data[ s->top ] );
+	free_matrix( transform );
+	break;
 
-    case ROTATE:
-      xval = op[i].op.rotate.degrees * ( M_PI / 180 );
+      case ROTATE:
+	printf("ROTATE\n");
+	xval = op[i].op.rotate.degrees * ( M_PI / 180 );
 
-      //get the axis
-      if ( op[i].op.rotate.axis == 0 ) 
-	transform = make_rotX( xval );
-      else if ( op[i].op.rotate.axis == 1 ) 
-	transform = make_rotY( xval );
-      else if ( op[i].op.rotate.axis == 2 ) 
-	transform = make_rotZ( xval );
+	//get the axis
+	if ( op[i].op.rotate.axis == 0 ) 
+	  transform = make_rotX( xval );
+	else if ( op[i].op.rotate.axis == 1 ) 
+	  transform = make_rotY( xval );
+	else if ( op[i].op.rotate.axis == 2 ) 
+	  transform = make_rotZ( xval );
 
-      matrix_mult( s->data[ s->top ], transform );
-      //put the new matrix on the top
-      copy_matrix( transform, s->data[ s->top ] );
-      free_matrix( transform );
-      break;
+	matrix_mult( s->data[ s->top ], transform );
+	//put the new matrix on the top
+	copy_matrix( transform, s->data[ s->top ] );
+	free_matrix( transform );
+	break;
 
-    case PUSH:
-      push( s );
-      break;
-    case POP:
-      pop( s );
-      break;
-    case SAVE:
-      save_extension( t, op[i].op.save.p->name );
-      break;
-    case DISPLAY:
-      display( t );
-      break;
+      case PUSH:
+	printf("PUSH\n");
+	push( s );
+	break;
+      case POP:
+	printf("POP\n");
+	pop( s );
+	break;
+      case SAVE:
+	printf("SAVE\n");
+	save_extension( t, op[i].op.save.p->name );
+	break;
+      case DISPLAY:
+	printf("DISPLAY\n");
+	display( t );
+	break;
+      }
     }
-  }
   
-  free_stack( s );
-  free_matrix( tmp );
-  //free_matrix( transform );
+    free_stack( s );
+    free_matrix( tmp );
+    //free_matrix( transform );
+  }
 }
